59d636ab33d9ed585e56274af33405f490d7afbc
Werozel <khaibulaev.mn@gmail.com>
2020-03-03 13:57:43 +0300

    Added some basic forms using gloss

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,19 @@
 module Main where
+import Graphics.Gloss
 
 import Lib
 
 main :: IO ()
-main = someFunc
+main = display getWindow white getElements
+
+
+getWindow :: Display
+getWindow = InWindow "Arcanoid" (300, 300) (20, 20)
+
+
+getCircle :: Float -> Picture
+getCircle x = ThickCircle (5*x) 5
+
+
+getElements :: Picture
+getElements = Pictures [circleSolid 5, translate 0 (-50) (rectangleSolid 20 5)]
