dd957165b24b95852d0f00039984797d31a6cdda
Cactiw <Knyazev1514@gmail.com>
2020-03-04 23:32:29 +0300

    Moved constants

--- src/Constants.hs
+++ src/Constants.hs
@@ -1,5 +1,6 @@
 module Constants where
 
+import Graphics.Gloss.Interface.Pure.Game
 
 -- Returns fps for the game
 fps :: Int
@@ -30,6 +31,16 @@ windowOffsetY = 100
 -- Returns grey coefficient for background color
 bgGreyN :: Float
 bgGreyN = 0.4
+
+-- Returns background color for window
+bgColor :: Color
+bgColor = greyN bgGreyN
+
+
+-- Returns window for the game
+window :: Display
+window = InWindow "Aracnoid" (windowWidth, windowHeight) (windowOffsetX, windowOffsetY)
+
 
 -- Returns speed coefficient for the ball
 speed :: Float
