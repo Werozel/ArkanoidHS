ae9286ed8f64446b756d88a3e83780b8c961b7c2
Werozel <khaibulaev.mn@gmail.com>
2020-03-04 23:30:58 +0300

    Moved initState to Run

--- src/Lib.hs
+++ src/Lib.hs
@@ -57,18 +57,4 @@ window :: Display
 window = InWindow "Aracnoid" (windowWidth, windowHeight) (windowOffsetX, windowOffsetY)
 
 
--- Returns initial game state
-initState :: Float -> GameState
-initState rnd = GameState False MainMenu initBallPos initBallDirection initPlatformPos 0 (BricksGrid [] NoHit) 1 NotFinished
-  where
-    initBallPos :: Point
-    initBallPos = (0, initBallPositionY)
-
-    initBallDirection :: Point
-    initBallDirection = (rnd, initBallDirectionY)
-
-    initPlatformPos :: Point
-    initPlatformPos = (0, initPlatformPositionY)
-
-
 
--- src/Run.hs
+++ src/Run.hs
@@ -12,6 +12,23 @@ import Base
 import LevelGenerator
 
 
+
+-- Returns initial game state
+initState :: Float -> GameState
+initState rnd = GameState False MainMenu initBallPos initBallDirection initPlatformPos 0 initGrid 0 NotFinished
+  where
+    initBallPos :: Point
+    initBallPos = (0, initBallPositionY)
+
+    initBallDirection :: Point
+    initBallDirection = (rnd, initBallDirectionY)
+
+    initPlatformPos :: Point
+    initPlatformPos = (0, initPlatformPositionY)
+    
+    initGrid = generateLevel 1
+
+
 run :: IO()
 run = do
   gen <- getStdGen
