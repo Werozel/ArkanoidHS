41949be77d3b603984cb4a85c948e6ef9ba488bf
Cactiw <Knyazev1514@gmail.com>
2020-03-07 22:48:14 +0300

    Added some tests

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,15 @@
+
+import System.Timeout
+import Run
+import Data.Maybe (isNothing)
+import System.Exit
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+-- main = putStrLn "Test suite not yet implemented"
+main = do
+  res <- timeout 5000 run
+  if isNothing res
+    then putStrLn "Game launched"
+    else exitWith (ExitFailure 1)
+
+
