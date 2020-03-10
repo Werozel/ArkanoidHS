51ddc394c92706b38e0c8912ebc68bdc6ea08157
OB11TO <OB11TO61@yandex.ru>
2020-03-10 14:22:10 +0300

    levelGenerator fix

--- src/LevelGenerator.hs
+++ src/LevelGenerator.hs
@@ -7,13 +7,6 @@ import Constants
 generateLevel :: Int -> BricksGrid
 generateLevel difficult = BricksGrid
                   [[Brick (-150, 100) (brickLength, brickHeight) 3,
-<<<<<<< HEAD
-                   Brick (0, 100.0) (brickLength, brickHeight) 3,
-                   Brick (150, 100.0) (brickLength, brickHeight) 3,
-                   Brick (0, 150.0 )(brickLength, brickHeight) 3,
-                   Brick (-100, 150.0 )(brickLength, brickHeight) 3,
-                   Brick (100, 150.0 )(brickLength, brickHeight) 3
-=======
                    Brick (-90, 100.0) (brickLength, brickHeight) 3,
                    Brick (-30, 100.0) (brickLength, brickHeight) 3,
                    Brick (30, 100.0) (brickLength, brickHeight) 3,
@@ -40,5 +33,4 @@ generateLevel difficult = BricksGrid
                    Brick (30, 170.0) (brickLength, brickHeight) 3,
                    Brick (90, 170.0) (brickLength, brickHeight) 3,
                    Brick (150, 170.0) (brickLength, brickHeight) 3
->>>>>>> 03c12f71528169c2dd4aee4d73324bb9676e119d
                   ]] NoHit
