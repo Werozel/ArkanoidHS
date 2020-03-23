45474532db95ca893e84f677b0a449fe92d5f602
OB11TO <OB11TO61@yandex.ru>
2020-03-23 19:08:01 +0300

    add menu

--- src/Run.hs
+++ src/Run.hs
@@ -61,11 +61,11 @@ tick _ state@GameState{..} | view /= LevelView = state
 -- Рисует картинку в окне для текущего состояния игры
 draw :: GameState -> Picture
 draw GameState {..} | view == StartScreen = Scale 0.25 0.25 $ Pictures [tutorialTextW,helloStr, tutorialTextContinue]
-                    | view == Menu = Scale 0.35 0.35 $ Pictures [menuTextRestrat,menuTextControl1,menuTwxtContinue ]
-                    | result == Win = Pictures [winText, winText2, winText3, nameGame,nameGame2, nameGame3, platform, wallsCollor, menu2, menu]
-                    | result == Lose = Pictures [loseText, loseText2, loseText3, nameGame, nameGame2, nameGame3, ball, platform, wallsCollor, menu2, menu]
+                    | view == Menu = Scale 0.45 0.45 $ Pictures [menuText,menuText2, menuTextControl, menuTextRestrat, menuTextPaused,menuTextContine,menuTextEsc,menuTextBonus,menuTextBonus2,nameGame4,nameGame5,nameGame6, menuTextBon]
+                    | result == Win = Pictures [winText, winText2, winText3, nameGame,nameGame2, nameGame3, platform, wallsCollor, menu2, menu, menuPaused, menuPausd2 , menuExit, menuExit2]
+                    | result == Lose = Pictures [loseText, loseText2, loseText3, nameGame, nameGame2, nameGame3, ball, platform, wallsCollor, menu2, menu, menuPaused, menuPausd2 , menuExit, menuExit2]
                     | view == Pause = Scale 0.50 0.50 $ Pictures [paused, paused2]
-                    | otherwise = Pictures [ball, nameGame, nameGame2, nameGame3, bricks, platform, wallsCollor, menu,menu2]
+                    | otherwise = Pictures [ball, nameGame, nameGame2, nameGame3, bricks, platform, wallsCollor, menu, menu2, menuPaused, menuPausd2 , menuExit, menuExit2]
                       where
                         paused = Translate (-windowWidthFloat * 0.67) 50 $ Color yellow $ Text "PAUSED"
                         paused2 = Translate (-windowWidthFloat * 0.66) 50 $ Color yellow $ Text "PAUSED"
@@ -74,17 +74,32 @@ draw GameState {..} | view == StartScreen = Scale 0.25 0.25 $ Pictures [tutorial
 
                         nameGame = Translate (-windowWidthFloat * 2.1 ) 320 $ Color azure $ Text "ARKANOID"
                         nameGame2 = Translate (-windowWidthFloat * 2.08 ) 324 $ Color magenta $ Text "ARKANOID"
-                        nameGame3 = Translate (-windowWidthFloat * 2.09 ) 323 $ Color cyan $ Text "ARKANOID"
-                        menu = Scale 0.25 0.25 $ Translate (-windowWidthFloat * 8.1) 150 $ Color magenta $ Text " Press m - to enter MENU"
-                        menu2 = Scale 0.25 0.25 $ Translate (-windowWidthFloat * 8.121) 151 $ Color magenta $ Text " Press m - to enter MENU"
+                        nameGame3 = Translate (-windowWidthFloat * 2.09 ) 324 $ Color cyan $ Text "ARKANOID"
+                        nameGame4 = Scale 2.3 2.3 $ Translate (-windowWidthFloat * 0.8 ) 310 $ Color azure $ Text "ARKANOID"
+                        nameGame5 = Scale 2.3 2.3 $ Translate (-windowWidthFloat * 0.78 ) 314 $ Color magenta $ Text "ARKANOID"
+                        nameGame6 = Scale 2.3 2.3 $ Translate (-windowWidthFloat * 0.79 ) 314 $ Color cyan $ Text "ARKANOID"
 
+                        menu = Scale 0.25 0.25 $ Translate (-windowWidthFloat * 8.1) 150 $ Color cyan $ Text "| Press 'M' - to enter MENU "
+                        menu2 = Scale 0.25 0.25 $ Translate (-windowWidthFloat * 8.109) 150 $ Color cyan $ Text "| Press 'M' - to enter MENU "
+                        menuPaused = Scale 0.25 0.25 $ Translate (-windowWidthFloat * 8.1) 0 $ Color cyan $ Text "| Press 'F' - to Paused     "
+                        menuPausd2 = Scale 0.25 0.25 $ Translate (-windowWidthFloat * 8.109) 0 $ Color cyan $ Text "| Press 'F' - to Paused  "
+                        menuExit = Scale 0.25 0.25 $ Translate (-windowWidthFloat * 8.1) (-150) $ Color cyan $ Text "| Press 'Esc' - to exit Game  "
+                        menuExit2 = Scale 0.25 0.25 $ Translate (-windowWidthFloat * 8.109) (-150) $ Color cyan $ Text "| Press 'Esc' - to exit Game  "
 
-                        menuTextRestrat = Translate (-windowWidthFloat * 1.5) 150 $ Color white $ Text "- Welcome"
-                        menuTextControl1 = Translate (-windowWidthFloat * 1.5) 0 $ Color white $ Text "to the game"
-                        menuTwxtContinue = Translate (-windowWidthFloat * 1.5) (-150) $ Color white $ Text "Press 'Space' to play"
+                        menuText =  Translate (-windowWidthFloat * 1.7) 370 $ Color yellow $ Text "Menu"
+                        menuText2 =  Translate (-windowWidthFloat * 1.69) 370 $ Color yellow $ Text "Menu"
+                        menuTextBonus =  Translate (-windowWidthFloat * 1.7) (-370) $ Color yellow $ Text "Bonus"
+                        menuTextBonus2 =  Translate (-windowWidthFloat * 1.69) (-370) $ Color yellow $ Text "Bonus"
+
+                        menuTextControl = Scale 0.70 0.70 $ Translate (-windowWidthFloat * 1.6) 300 $ Color white $ Text "- control arrow  <- | ->"
+                        menuTextRestrat = Scale 0.70 0.70 $ Translate (-windowWidthFloat * 1.6) 150 $ Color white $ Text "- 'R' restart game"
+                        menuTextPaused = Scale 0.70 0.70 $ Translate (-windowWidthFloat * 1.6) 0 $ Color white $ Text "- 'F' paused game"
+                        menuTextEsc = Scale 0.70 0.70 $ Translate (-windowWidthFloat * 1.6) (-150) $ Color white $ Text "- 'Esc' to exit game "
+                        menuTextContine = Scale 0.70 0.70 $ Translate (-windowWidthFloat * 1.6) (-300) $ Color white $ Text "- Press 'Space' to play"
+                        menuTextBon = Scale 0.70 0.70 $  Translate (-windowWidthFloat * 1.6) (-750) $ Color white $ Text "Hitting a corner of a brick - one shot it"
 
                         tutorialTextW = Translate (-windowWidthFloat * 2.5) 110 $ Color white $ Text "Welcome to the game ARKANOID!"
-                        tutorialTextContinue = Translate (-windowWidthFloat * 1.5) (-100) $ Color white $ Text "Press 'Space' to play"
+                        tutorialTextContinue = Translate (-windowWidthFloat * 1.5) (-100) $ Color white $ Text "Press 'Enter' to play"
 
                         winText = Translate (- windowWidthFloat / 4) 0 $ Color yellow $ Text "Win!"
                         winText2 = Translate (- windowWidthFloat / 4) 2.6 $ Color yellow $ Text "Win!"
@@ -122,7 +137,7 @@ eventHandler :: Event -> GameState -> GameState
 eventHandler (EventKey (SpecialKey key) keyState _ _) state@GameState {..}
   |key == KeySpace && view == Menu = state {view = LevelView}
   |key == KeySpace && view == Pause = state{view = LevelView}
-  |key == KeySpace && view == StartScreen = state{view = LevelView}   -- Handles continue while in start screen
+  |key == KeyEnter && view == StartScreen = state{view = Menu}   -- Handles continue while in start screen
   | view /= LevelView = state   -- Фильтрует все входные сигналы если не играет прямо сейчас
   -- Left arrow key moves platform to the left
   | key == KeyLeft = state {keysPressed = if keyState == Down then LeftPressed:keysPressed else delete LeftPressed keysPressed}
@@ -142,6 +157,6 @@ eventHandler _ state = state
 run :: IO()
 run = do
   putStrLn "Hello, what's your name?"
-  name <-getLine
+
   gen <- getStdGen
   play  window bgColor fps (initState (fst (randomR randRange gen ))  StartScreen) draw eventHandler tick
