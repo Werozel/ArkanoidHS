8c5d9424fa33e6cf11348fead7ab2d0f7e61705d
Cactiw <Knyazev1514@gmail.com>
2020-04-08 22:30:14 +0300

    Changed test signatures to IO

--- test/Spec.hs
+++ test/Spec.hs
@@ -10,13 +10,17 @@ import Base
 
 main :: IO ()
 -- main = putStrLn "Тестовый набор еще не реализован"
-main
-  | getRemainingBricksCount (grid res) >= 0 = putStrLn "Test passed"
-  | otherwise = exitWith (ExitFailure 10)
-  where
-    res = gameTest (initState 25 LevelView) tick (testSeconds * fps)
+main = do
+  initState <- initState "Test" 25 LevelView
+  res <- gameTest initState tick (testSeconds * fps)
+  if getRemainingBricksCount (grid res) >= 0
+    then putStrLn "Test passed"
+    else exitWith (ExitFailure 10)
+
 
 -- Функция, вызывающая время тиковых шагов
-gameTest :: GameState -> (Float -> GameState -> GameState) -> Int -> GameState
-gameTest state@GameState{..} tick 0 = state
-gameTest state@GameState{..} tick steps = gameTest (tick 0 state) tick (steps - 1)
+gameTest :: GameState -> (Float -> GameState -> IO GameState) -> Int -> IO GameState
+gameTest state@GameState{..} tick 0 = return state
+gameTest state@GameState {..} tick steps = do
+  newState <- tick 0 state
+  gameTest newState tick (steps - 1)
