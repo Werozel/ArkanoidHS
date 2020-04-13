d8a33874fa2e293de7a3b00dfb966bbcf22999eb
Cactiw <Knyazev1514@gmail.com>
2020-04-13 14:46:04 +0300

    Changed tests performing method to new Test class

--- test/Spec.hs
+++ test/Spec.hs
@@ -7,15 +7,50 @@ import System.Exit
 import Lib
 import Constants
 import Base
+import Test
+
+getTests :: IO [Test]
+getTests = do
+  beginState <- initState "Test" 25 LevelView
+  return [ Test "Стабильность работы" beginState Run.tick 0 (testSeconds * fps) doNothing True gameNotCrashed ]
+
 
 main :: IO ()
 -- main = putStrLn "Тестовый набор еще не реализован"
 main = do
-  initState <- initState "Test" 25 LevelView
-  res <- gameTest initState tick (testSeconds * fps)
-  if getRemainingBricksCount (grid res) >= 0
-    then putStrLn "Test passed"
-    else exitWith (ExitFailure 10)
+  tests <- getTests
+  res <- runAllTests tests
+  if res
+    then putStrLn "All test passed"
+    else do 
+      putStrLn "Some tests failed"
+      exitWith (ExitFailure 10)
+
+
+runAllTests :: [Test] -> IO Bool
+runAllTests [] = return True
+runAllTests (test@Test{..}:xs) = do 
+  thisResult <- runTest test
+  otherResult <- runAllTests xs
+  return (thisResult && otherResult)
+
+doNothing :: GameState -> IO GameState
+doNothing = return
+
+gameNotCrashed :: Test -> Bool
+gameNotCrashed test@Test{..} = getRemainingBricksCount (grid state) >= 0
+
+runTest :: Test -> IO Bool
+runTest test@Test {..}
+  | tickCount >= tickLimit = return (checkSuccess test)
+  | otherwise = do
+    newState <- tick 0 state
+    finalState <- testTick state
+--    if (not waitAll) and (checkSuccess test)
+    if not waitAll && checkSuccess test
+      then return True
+      else runTest test {state = finalState, tickCount = tickCount + 1}
+
 
 
 -- Функция, вызывающая время тиковых шагов
